fe3af9ed56f65b2b03264812928d39c5badcb002
Katarina Milovanovic <k.milovanovic55@gmail.com>
2024-08-05 23:14:44 +0200

    Dodato brojanje koraka

--- step-counter/App.js
+++ step-counter/App.js
@@ -17,6 +17,45 @@ export default function App() {
   const animationRefSitting = useRef(null);
 
 
+  useEffect(() =>{
+    let subscription;
+    Accelerometer.isAvailableAsync().then((result) => {
+      if(result){
+        subscription = Accelerometer.addListener((accelerometer) => {
+          const {y} = accelerometerData;
+          const threshold = 0.1;
+          const timestamp = new Date().getTime();
+
+          if(
+            Math.abs(y - lastY) > threshold && !isCounting && (timestamp - lastTimestamp > 8000)
+          ) {
+            setIsCounting(true);
+            setLastY(y);
+            setLastTimestamp(timestamp);
+
+            setSteps((prevSteps) => prevSteps +1);
+            
+            setTimeout(() => {
+              setIsCounting(false);
+            }, 1200);
+          }
+        });
+      } else {
+        console.log('Accelerometer not available on this device');
+      }
+    });
+
+    return () => {
+      if(subscription) {
+        subscription.remove();
+      }
+    };
+  }, [isCounting, lastY, lastTimestamp]);
+
+  const resetSteps = () => {
+    setSteps(0);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
