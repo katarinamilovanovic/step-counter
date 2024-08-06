78f7f40a02cc28801d0384c02f8e326166de48d3
Katarina Milovanovic <k.milovanovic55@gmail.com>
2024-08-06 12:41:08 +0200

    Dodat front, Lottie animacija

--- step-counter/App.js
+++ step-counter/App.js
@@ -5,6 +5,7 @@ import { Accelerometer } from 'expo-sensors';
 import { Constants } from 'expo-constants';
 import LottieView from 'lottie-react-native';
 
+const CALORIES_PER_STEP = 0.05;
 
 export default function App() {
 
@@ -15,7 +16,6 @@ export default function App() {
 
   const animationRefRunning = useRef(null);
   const animationRefSitting = useRef(null);
-
 
   useEffect(() =>{
     let subscription;
@@ -56,11 +56,43 @@ export default function App() {
     setSteps(0);
   };
 
+  const estimatedCaloriesBurned = steps*CALORIES_PER_STEP;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style = {styles.title}>Step Tracker</Text>
+      <View style={styles.infoContainer}>
+        <View style = {styles.stepsContainer}>
+          <Text style = {styles.stepsText}>{steps}</Text>
+          <Text style = {styles.stepsLabel}>Steps</Text>
+        </View>
+        <View style = {styles.caloriesContainer}>
+          <Text style = {styles.caloriesLabel}>
+            Estimated Calories Burned:
+          </Text>
+          <Text style={styles.caloriesText}>
+            {estimatedCaloriesBurned.toFixed(2)} calories
+          </Text>
+        </View>
+      </View>
+      <View style={styles.animationContainer} >
+        {isCounting? (
+          <LottieView 
+            autoPlay
+            ref={animationRefRunning}
+            style={styles.animation}
+            source={require('./assets/walking.json')}
+          />
+        ) : (
+          <LottieView
+          autoPlay
+          ref={animationRefSitting}
+          style={styles.animation}
+          source={require('./assets/sitting.json')}
+          />
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -71,4 +103,58 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    fontSize: 28,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    color: "#333"
+  },
+  informationContainer: {
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  stepsContainer :{
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 20
+  },
+  stepsText: {
+    fontSize: 36,
+    color: '#3498db',
+    fontWeight: 'bold',
+    marginRight: 8
+  },
+  stepsLabel: {
+    fontSize: 24,
+    color: '#555'
+  },
+  caloriesContainer:{
+    flexDirection:'row',
+    alignItems:'center',
+    marginBottom: 20
+  },
+  caloriesLabel:{
+    fontSize: 20,
+    color: '#555',
+    marginRight:6
+  },
+  caloriesText:{
+    fontSize: 18,
+    color:'#e74c3c',
+    fontWeight:'bold'
+  },
+  animationContainer:{
+    alignItems: 'center',
+    justifyContent:'center',
+    backgroundColor: '#e0e0e0',
+    borderRadius:15,
+    padding: 20,
+    marginBottom:20,
+    elevation: 5
+  },
+  animation:{
+    width: 400,
+    height: 400,
+    backgroundColor: 'transparent'
+  }
 });
